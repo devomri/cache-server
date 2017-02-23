0b6e7655e9fa0567a140afaf6e2f61aae64e4276
Omri A <devomri@gmail.com>
2017-02-23 21:08:07 +0200

    cache

---
+++ Model/Cache.js
@@ -0,0 +1,11 @@
+/**
+ * Created by omri on 2/23/17.
+ */
+
+class Cache {
+    constructor(ID, cpacity) {
+        this.ID = ID;
+        this.capacity = capacity;
+        this.avaiableCapacity = capacity;
+    }
+}
