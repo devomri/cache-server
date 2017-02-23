3028348b36ca019c98f08b8760372b42760e426b
Ori Riner <oririner@gmail.com>
2017-02-23 21:41:10 +0200

    Sort and iterate videos

--- main.js
+++ main.js
@@ -4,29 +4,37 @@ const parse = require('./parser');
 let wasVideoAdd = false;
 let objectsModel = {};
 
-function iterateCaches() {
+function iterateCaches(sortedCachesForVideo) {
   // try to put the video on that cache
   // update the cache server and move on to the next video
   // mark a video that was added in this iteration
 }
 
-function sortCachesForVideo() {
+function sortCachesForVideo(cachesForVideo) {
   // sort all the distinct caches according to the most common accessible cache from all the endpoints
 }
 
 function iterateVideos(){
-  // for each video
-  // take all the endpoints that request it.
-  sortCachesForVideo();
-  iterateCaches();
+  objectsModel.sortedVideos.forEach((video) => {
+    const cachesForVideo = Object.keys(objectsModel.endpointsByVideo[video.id].reduce((caches, endpoint) => {
+      endpoint.cacheLatencyList.forEach(c => caches[c.cacheID]);
+      return caches;
+    }, {}));
+
+    const sortedCachesForVideo = sortCachesForVideo(cachesForVideo);
+    iterateCaches(sortedCachesForVideo);
+
+  });
 }
 
 function Main() {
   fs.readFile('./input.txt', (input) => {
     objectsModel = parse(input);
 
-    objectsModel.SortedVideos = objectsModel.requests.sort((videoA, videoB) => {
-
+    objectsModel.sortedVideos = objectsModel.videoes.sort((videoA, videoB) => {
+      const videoATotalRequests = objectsModel.requestsByVideo[videoA.ID].reduce((total, request) => request.iterations + total, 0);
+      const videoBTotalRequests = objectsModel.requestsByVideo[videoB.ID].reduce((total, request) => request.iterations + total, 0);
+      return videoATotalRequests - videoBTotalRequests;
     });
 
     while (true) {
